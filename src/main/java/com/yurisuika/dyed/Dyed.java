d89bf068735d5e30903cd9e70acb625447133fbe
yurisuika <yurisuika@protonmail.com>
2022-06-15 00:03:39 -0700

    SLF4J Again on 1.19

--- src/main/java/com/yurisuika/dyed/Dyed.java
+++ src/main/java/com/yurisuika/dyed/Dyed.java
@@ -1,16 +1,16 @@
 package com.yurisuika.dyed;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 @Mod("dyed")
 public class Dyed
 {
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public Dyed()
     {
