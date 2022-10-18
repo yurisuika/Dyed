489c0f4ac2436326ec3c821633807594f451fce0
yurisuika <yurisuika@protonmail.com>
2022-10-18 12:01:09 -0700

    Simplified into one mixin

--- src/main/java/com/yurisuika/dyed/mixin/client/renderer/entity/layers/LeatherHorseArmorLayerMixin.java
+++ src/main/java/com/yurisuika/dyed/mixin/client/renderer/entity/layers/LeatherHorseArmorLayerMixin.java
@@ -1,13 +1,11 @@
-package com.yurisuika.dyed.client.renderer.entity.layers;
+package com.yurisuika.dyed.mixin.client.renderer.entity.layers;
 
 import com.google.common.collect.Maps;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.IEntityRenderer;
-import net.minecraft.client.renderer.entity.layers.LayerRenderer;
-import net.minecraft.client.renderer.entity.model.HorseModel;
+import net.minecraft.client.renderer.entity.layers.LeatherHorseArmorLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.passive.horse.HorseEntity;
 import net.minecraft.item.DyeableHorseArmorItem;
@@ -17,31 +15,27 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.apache.commons.lang3.StringUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
 import java.util.Map;
 
 @OnlyIn(Dist.CLIENT)
-public class DyedLeatherHorseArmorLayer extends LayerRenderer<HorseEntity, HorseModel<HorseEntity>> {
-    private final HorseModel<HorseEntity> model = new HorseModel<>(0.1F);
-
+@Mixin(LeatherHorseArmorLayer.class)
+public class LeatherHorseArmorLayerMixin {
     private static final Map<String, ResourceLocation> HORSE_ARMOR_TEXTURE_CACHE = Maps.newHashMap();
 
-    public DyedLeatherHorseArmorLayer(IEntityRenderer<HorseEntity, HorseModel<HorseEntity>> p_i50937_1_) {
-        super(p_i50937_1_);
-    }
-
-    public void render(MatrixStack p_225628_1_, IRenderTypeBuffer p_225628_2_, int p_225628_3_, HorseEntity p_225628_4_, float p_225628_5_, float p_225628_6_, float p_225628_7_, float p_225628_8_, float p_225628_9_, float p_225628_10_) {
-        this.renderHorseArmor(p_225628_1_, p_225628_2_, p_225628_3_, p_225628_4_, p_225628_5_, p_225628_6_, p_225628_7_, p_225628_8_, p_225628_9_, p_225628_10_);
-    }
-
-    private void renderHorseArmor(MatrixStack p_225628_1_, IRenderTypeBuffer p_225628_2_, int p_225628_3_, HorseEntity p_225628_4_, float p_225628_5_, float p_225628_6_, float p_225628_7_, float p_225628_8_, float p_225628_9_, float p_225628_10_) {
+    @Inject(method = "render(Lcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;ILnet/minecraft/entity/passive/horse/HorseEntity;FFFFFF)V", at = @At("TAIL"))
+    public void injectRender(MatrixStack p_225628_1_, IRenderTypeBuffer p_225628_2_, int p_225628_3_, HorseEntity p_225628_4_, float p_225628_5_, float p_225628_6_, float p_225628_7_, float p_225628_8_, float p_225628_9_, float p_225628_10_, CallbackInfo ci) {
         ItemStack itemstack = p_225628_4_.getArmor();
         if (itemstack.getItem() instanceof HorseArmorItem) {
             HorseArmorItem horsearmoritem = (HorseArmorItem)itemstack.getItem();
-            this.getParentModel().copyPropertiesTo(this.model);
-            this.model.prepareMobModel(p_225628_4_, p_225628_5_, p_225628_6_, p_225628_7_);
-            this.model.setupAnim(p_225628_4_, p_225628_5_, p_225628_6_, p_225628_8_, p_225628_9_, p_225628_10_);
+            ((LeatherHorseArmorLayer)(Object)this).getParentModel().copyPropertiesTo(((LeatherHorseArmorLayer)(Object)this).model);
+            ((LeatherHorseArmorLayer)(Object)this).model.prepareMobModel(p_225628_4_, p_225628_5_, p_225628_6_, p_225628_7_);
+            ((LeatherHorseArmorLayer)(Object)this).model.setupAnim(p_225628_4_, p_225628_5_, p_225628_6_, p_225628_8_, p_225628_9_, p_225628_10_);
             if (horsearmoritem instanceof DyeableHorseArmorItem) {
                 int i = ((DyeableHorseArmorItem)horsearmoritem).getColor(itemstack);
                 float n = (float)(i >> 16 & 255) / 255.0F;
@@ -57,7 +51,7 @@ public class DyedLeatherHorseArmorLayer extends LayerRenderer<HorseEntity, Horse
 
     private void renderHorseArmorParts(MatrixStack p_225628_1_, IRenderTypeBuffer p_225628_2_, int light, HorseArmorItem name, float red, float green, float blue, @Nullable String overlay) {
         IVertexBuilder ivertexbuilder = p_225628_2_.getBuffer(RenderType.armorCutoutNoCull(this.getHorseArmorTexture(name, overlay)));
-        model.renderToBuffer(p_225628_1_, ivertexbuilder, light, OverlayTexture.NO_OVERLAY, red, green, blue, 1.0F);
+        ((LeatherHorseArmorLayer)(Object)this).model.renderToBuffer(p_225628_1_, ivertexbuilder, light, OverlayTexture.NO_OVERLAY, red, green, blue, 1.0F);
     }
 
     private ResourceLocation getHorseArmorTexture(HorseArmorItem name, @Nullable String overlay) {
