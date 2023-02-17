d49733ef46ac6a5868da856589b288d8b3cec5ad
yurisuika <yurisuika@protonmail.com>
2023-02-17 10:43:06 -0800

    Removed logger

--- src/main/java/dev/yurisuika/dyed/Dyed.java
+++ src/main/java/dev/yurisuika/dyed/Dyed.java
@@ -2,23 +2,12 @@ package dev.yurisuika.dyed;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod("dyed")
 public class Dyed {
 
-    private static final Logger LOGGER = LogManager.getLogger();
-
     public Dyed() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-        LOGGER.info("Loading Dyed!");
     }
 
 }
