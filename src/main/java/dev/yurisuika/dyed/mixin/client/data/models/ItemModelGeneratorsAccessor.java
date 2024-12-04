ec922bdcf5416afd5038f79a09b36e199b53b382
yurisuika <yurisuika@protonmail.com>
2024-12-04 00:45:01 -0800

    1.21.4

---
+++ src/main/java/dev/yurisuika/dyed/mixin/client/data/models/ItemModelGeneratorsAccessor.java
@@ -0,0 +1,21 @@
+package dev.yurisuika.dyed.mixin.client.data.models;
+
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.ItemModelOutput;
+import net.minecraft.client.data.models.model.ModelInstance;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.function.BiConsumer;
+
+@Mixin(ItemModelGenerators.class)
+public interface ItemModelGeneratorsAccessor {
+
+    @Accessor
+    ItemModelOutput getItemModelOutput();
+
+    @Accessor
+    BiConsumer<ResourceLocation, ModelInstance> getModelOutput();
+
+}
