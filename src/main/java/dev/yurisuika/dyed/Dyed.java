0951c8ed63a9956037014cd27c835344f190005e
yurisuika <yurisuika@protonmail.com>
2024-12-27 02:41:51 -0800

    restructure with inner client class

--- src/main/java/dev/yurisuika/dyed/Dyed.java
+++ src/main/java/dev/yurisuika/dyed/Dyed.java
@@ -2,8 +2,14 @@ package dev.yurisuika.dyed;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("dyed")
 public class Dyed {
+
+    @Mod("dyed")
+    public static class Client {
+
+        public Client() {}
+
+    }
 
     public Dyed() {}
 
