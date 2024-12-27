6da904e21e1a90e37fbc58907437c2349122ed42
yurisuika <yurisuika@protonmail.com>
2024-12-27 01:09:02 -0800

    remove unneeded event bus register

--- src/main/java/dev/yurisuika/dyed/Dyed.java
+++ src/main/java/dev/yurisuika/dyed/Dyed.java
@@ -1,13 +1,10 @@
 package dev.yurisuika.dyed;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("dyed")
 public class Dyed {
 
-    public Dyed() {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+    public Dyed() {}
 
 }
