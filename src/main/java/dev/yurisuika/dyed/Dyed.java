1bcb2f5cea194e992f20f7597c95f616eaf730c2
yurisuika <yurisuika@protonmail.com>
2024-12-27 04:27:52 -0800

    add client dist marker

--- src/main/java/dev/yurisuika/dyed/Dyed.java
+++ src/main/java/dev/yurisuika/dyed/Dyed.java
@@ -1,10 +1,11 @@
 package dev.yurisuika.dyed;
 
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 
 public class Dyed {
 
-    @Mod("dyed")
+    @Mod(value = "dyed", dist = Dist.CLIENT)
     public static class Client {
 
         public Client() {}
